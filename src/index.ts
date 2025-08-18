192b10bcbf3f01a73d18943a3e291ddb35ee754a
hydraa94 <nailuldev2948@gmail.com>
2025-08-18 10:39:50 +0700

    Re-init hono
    Initializing hoono again because the old one is using aws lambda template which is stupid bcs we dont event have money to use it

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,9 @@
-import "dotenv/config";
-import { Hono } from "hono";
-import { serve } from "@hono/node-server";
-import { env } from "./env";
-import { health } from "./routes/health";
-import { supabase } from "./lib/supabase";
-import { cors } from 'hono/cors'
+import { Hono } from 'hono'
 
-const app = new Hono();
+const app = new Hono()
 
-app.use('*', cors())
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
 
-// Map route health check
-app.route("/", health);
-
-// Map post route
-app.get("/posts", async (c) => {
-  const { data, error } = await supabase
-    .from("posts")
-    .select("id,title,content,created_at")
-    .order("created_at", { ascending: false });
-  if (error) return c.json({ error: error.message }, 500);
-  return c.json(data);
-});
-
-export default app;
-
-console.log(`> API running on http://localhost:${env.PORT}`);
-serve({ fetch: app.fetch, port: env.PORT });
+export default app
